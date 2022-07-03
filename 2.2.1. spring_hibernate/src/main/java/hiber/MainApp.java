06a34b36184fbca83137f46d1d455600231bc935
Serg183 <sergmygit@gmail.com>
2022-07-04 03:37:42 +1000

    commit 2

--- 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
+++ 2.2.1. spring_hibernate/src/main/java/hiber/MainApp.java
@@ -35,10 +35,10 @@ public class MainApp {
 
       List<User> users = userService.getUsersCar("car3", 333);
       for (User user : users) {
-         System.out.println("Id = "+user.getId());
-         System.out.println("First Name = "+user.getFirstName());
-         System.out.println("Last Name = "+user.getLastName());
-         System.out.println("Email = "+user.getEmail());
+         System.out.println("Id = " + user.getId());
+         System.out.println("First Name = " + user.getFirstName());
+         System.out.println("Last Name = " + user.getLastName());
+         System.out.println("Email = " + user.getEmail());
          System.out.println();
       }
 
